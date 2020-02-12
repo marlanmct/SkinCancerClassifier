59856e9ee447b85f01496c78d71018f52bcd3be6
Marlan McInnes-Taylor <contact@marlanmcinnestaylor.com>
2020-02-12 09:33:28 -0500

    initial implementation of app

--- src/scc.py
+++ src/scc.py
@@ -1,7 +1,46 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = '../uploads/'
+# ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+ALLOWED_EXTENSIONS = {'jpg', 'jpeg'}
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            flash('No selected file')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            # filename = secure_filename(file.filename)
+            # file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('results',
+                                    filename=file.filename))
+    return render_template('index.html')
+
+
+@app.route('/results/<filename>')
+def results(filename):
+    return render_template('results.html')
+    
+    #send_from_directory(app.config['UPLOAD_FOLDER'],
+    #                           filename)
