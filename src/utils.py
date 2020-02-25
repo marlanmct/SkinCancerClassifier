cb9b9accd42af58db478b566c01206b7041de0b3
Marlan McInnes-Taylor <contact@marlanmcinnestaylor.com>
2020-02-25 15:50:17 -0500

    creates dictionary for results printing

--- src/utils.py
+++ src/utils.py
@@ -2,3 +2,10 @@
 def allowed_file(filename, ALLOWED_EXTENSIONS):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+def addClassNames(classes, results):
+    classPercent = {} # dict to store full class names and match percentage
+    for key in results.keys():
+        classPercent[key] = (classes[key], results[key])
+    
+    return classPercent
