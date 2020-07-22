1461021907a3af66803d024d138620fa18477b76
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-07-21 22:49:14 -0400

    video feed

--- src/app.py
+++ src/app.py
@@ -1,11 +1,29 @@
-import os
-import src.config as config 
-import src.utils as utils
-import src.model.label_image as model
-import src.model.classes as classes
-from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
+import cv2, os, threading, time
+from flask import Flask, Response, flash, request, redirect, url_for, render_template, send_from_directory, Response
 from werkzeug.utils import secure_filename
 
+if __name__ == '__main__':
+    import config as config 
+    import utils as utils
+    import model.label_image as model
+    import model.classes as classes
+else:
+    import src.config as config
+    import src.utils as utils
+    import src.model.label_image as model
+    import src.model.classes as classes
+
+
+# GStreamer Pipeline to access the Raspberry Pi camera
+GSTREAMER_PIPELINE = 'nvarguscamerasrc ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
+
+# Image frame sent to the Flask object
+global video_frame
+video_frame = None
+
+# Use locks for thread-safe viewing of frames in multiple browsers
+global thread_lock
+thread_lock = threading.Lock()
 
 UPLOAD_FOLDER = config.uploads
 ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png'}
@@ -14,6 +32,64 @@ app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
+
+def captureFrames():
+    global video_frame, thread_lock
+
+    # Video capturing from OpenCV
+    video_capture = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
+
+    while True and video_capture.isOpened():
+        return_key, frame = video_capture.read()
+        if not return_key:
+            break
+
+        # Create a copy of the frame and store it in the global variable,
+        # with thread safe access
+        with thread_lock:
+            video_frame = frame.copy()
+
+        key = cv2.waitKey(30) & 0xff
+        if key == 27:
+            break
+
+    video_capture.release()
+
+
+def encodeFrame():
+    global thread_lock
+    while True:
+        # Acquire thread_lock to access the global video_frame object
+        with thread_lock:
+            global video_frame
+            if video_frame is None:
+                continue
+            return_key, encoded_image = cv2.imencode(".jpg", video_frame)
+            if not return_key:
+                continue
+
+        # Output image as a byte array
+        yield(b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+              bytearray(encoded_image) + b'\r\n')
+
+
+def saveFrame():
+    global thread_lock
+     with thread_lock:
+        global video_frame
+        if video_frame is None:
+            continue
+        return_key, encoded_image = cv2.imencode(".jpg", video_frame)
+        if not return_key:
+            continue
+
+        
+# Create a thread and attach the method that captures the image frames, to it
+process_thread = threading.Thread(target=captureFrames)
+process_thread.daemon = True
+
+# Start the thread
+process_thread.start()
 
 @app.errorhandler(404)
 
@@ -64,6 +140,11 @@ def uploads(filename):
 @app.route('/links/')
 def links():
     return render_template('links.html', email=config.email, github=config.github)
+
+
+@app.route('/video_feed/')
+def streamFrames():
+    return Response(encodeFrame(), mimetype="multipart/x-mixed-replace; boundary=frame")
 
 
 def not_found(e):
