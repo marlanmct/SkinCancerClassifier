df955dd7d2aa707ff5070e8b7b1c0ab7c2e46f28
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-07-26 15:16:06 -0400

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -5,7 +5,19 @@ def allowed_file(filename, ALLOWED_EXTENSIONS):
 
 def addClassNames(classes, results):
     classPercent = {} # dict to store full class names and match percentage
-    for key in results.keys():
-        classPercent[key] = (classes[key], results[key])
-    
+    for i, result in enumerate(results):
+        if i == 0:
+            classPercent['akiec'] = (classes['akiec'], result)
+        elif i == 1:
+            classPercent['bcc'] = (classes['bcc'], result)
+        elif i == 2:
+            classPercent['bkl'] = (classes['bkl'], result)
+        elif i == 3:
+            classPercent['df'] = (classes['df'], result)
+        elif i == 4:
+            classPercent['mel'] = (classes['mel'], result)
+        elif i == 5:
+            classPercent['nv'] = (classes['nv'], result)
+        else:
+            classPercent['vasc'] = (classes['vasc'], result)
     return classPercent
