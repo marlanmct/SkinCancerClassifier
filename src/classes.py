73ceb48ab464a0f37b55f4c3a78cf20ed73c40ee
Marlan McInnes-Taylor <mmcinnestaylor@gmail.com>
2020-07-26 15:13:20 -0400

    moving file

---
+++ src/classes.py
@@ -0,0 +1,3 @@
+classNames = {'mel':'Melanoma', 'nv': 'Melanocytic nevus',
+           'bcc':'Basal cell carcinoma', 'akiec':'Actinic keratosis / Bowen’s disease', 'bkl':'Benign keratosis', 'df':'Dermatofibroma', 'vasc':'Vascular lesion'}
+           
--- src/model/classes.py
+++
@@ -1,2 +0,0 @@
-classNames = {'mel':'Melanoma', 'nv': 'Melanocytic nevus',
-           'bcc':'Basal cell carcinoma', 'akiec':'Actinic keratosis / Bowen’s disease', 'bkl':'Benign keratosis', 'df':'Dermatofibroma', 'vasc':'Vascular lesion'}
